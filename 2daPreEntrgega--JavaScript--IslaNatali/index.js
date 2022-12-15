8175f07f6801f86241fda474ca93efac6a51558c
Natali Isla <natali.isla.njib@gmail.com>
2022-12-14 21:50:05 -0500

    Final correccion

--- 2daPreEntrgega--JavaScript--IslaNatali/index.js
+++ 2daPreEntrgega--JavaScript--IslaNatali/index.js
@@ -1,24 +1,68 @@
 'use strict';
 
-// Push de Servicios
+let fullName = prompt('Ingresa nombre y apellido o razón social');
+prompt('Ingresa RUC o DNI');
+alert(
+  'Conoce nuestros servicios logísticos:\n1. Express (3h)\n2. Sameday (24h)\n3. Next day (48h)'
+);
+prompt('Ingresa el número de envios que requieres al mes');
 
-const MARCA = "Chazki - Logística de Punta a Punta"
-const BIENVENIDA = ("¡Bienvenido a la " + MARCA + "!");
+let history = [];
 
-console.log(MARCA);
-alert(MARCA);
-
-let services = [];
-
-console.log(services);
-
-let end;
+let end = false;
 
 while (!end) {
-  let service = prompt('Que servicio necesitas?');
-  services.push(service);
-  end = confirm('Eso es todo?');
-  console.log(services);
+  let service = Number(
+    prompt(
+      'Escoje cual de nuestros servicios deseas  cotizar:\n1. Express (3h)\n2. Sameday (24h)\n3. Next day (48h)'
+    )
+  );
+
+  if (service) {
+    let packageByService = Number(
+      prompt(
+        'Indica el numero de envios por este servicio \n *Tener en cuenta que el cáculo contempla los primeros 3km de la ruta de envio'
+      )
+    );
+
+    let price = 0;
+    let serviceName = '';
+
+    switch (service) {
+      case 1:
+        price = 5 * packageByService;
+        serviceName = 'EXPRESS';
+        break;
+      case 2:
+        price = 4 * packageByService;
+        serviceName = 'SAMEDAY';
+        break;
+      case 3:
+        price = 3 * packageByService;
+        serviceName = 'NEXTDAY';
+        break;
+      default:
+        serviceName = null;
+        break;
+    }
+
+    alert(`El costo de tu servicio mensual aproximado es de: ${price}  \n Si la orden genera rutas de mas de 3km,la tarifa incluiria costo adicional`);
+
+    if (serviceName) {
+      history.push(`${serviceName} => ${price}\n`);
+    }
+
+    end = !confirm('Algun otro servicio a contratar?');
+  } else {
+    break;
+  }
 }
 
-console.log('TODOS', services);
+function Clousure(history,fullName){if (history.length > 0) {
+  alert(`Gracias por tu contacto, este es tu resumen:\n${history.join('')}`);
+} else {
+  alert(`Gracias por tu contacto ${fullName}`);
+}
+}
+
+Clousure(history,fullName)
---
+++ Dieguito/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/* console.log('Hello World!');
+console.log(1);
+
+alert('Hello World!')
+
+let message='Hello World!';
+console.log(message);
+
+alert(message) */
+
+let fullName = prompt('Ingresa nombre completo o razón social');
+let documentNumber = prompt('Ingresa RUC o DNI');
+
+alert('Conoce nuestro servicios logiticos: \n1. Express (3h) \n2. Same Day (24h) \n3. Next Day (48h)');
+
+let packageQuantity = prompt('Ingresa el Nro de envîos que requieres al mes');
+
+let service = prompt('Escoje cual de nuestros servicios deseas cotizar:\n1. Express (3h) \n2. Same Day (24h) \n3. Next Day (48h)\n4. Salir')
+
+if (service !== null) {
+    let packageByService = Number(
+        
+    )
+}
+let packagByService = Number(
+    prompt(
+        'Indica el numero de envios por este servicio'
+    )
+);
+
+alert('El costo de tus envios en este servicio mensual seria de:');
+
+
+
+
+
+
+
